d0c92b9b9581f9d956007599288a72e26c14f8aa
mohammedrehan275 <mohammedrehan275@gmail.com>
2023-02-16 16:59:25 +0530

    fix: simplifying db connectivity using mongoose

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/connect");
-// const {connectToServer} = require("./db/connect");
- 
-app.get('/', (req, res) => res.send('Hello world!'));
 
-app.listen(port, async () => {
-  console.log("server")
-  //perform a database connection when server starts
-  await dbo.connectToServer(function (err) {
-    console.log("connecting to server")
-    if (err) console.error(err);
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.ATLAS_URI;
+
+const routes = require('./routes/record');
+
+//setting mongoDB schema to strict mode
+mongoose.set("strictQuery", false);
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+//runs only once to display database connectivity
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`)
+})
+
+//routes
+app.use('/api', routes)
